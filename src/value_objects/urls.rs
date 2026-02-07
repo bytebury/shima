0030ed049ed5b31875e639aef2dc87ad6930a806
Marcello <marchsabino@gmail.com>
2026-02-07 16:41:45 -0500

    feat: use cow for urls

--- src/value_objects/urls.rs
+++ src/value_objects/urls.rs
@@ -1,31 +1,50 @@
 use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 #[serde(transparent)]
-pub struct CancelUrl<'a>(&'a str);
+pub struct CancelUrl<'a>(Cow<'a, str>);
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 #[serde(transparent)]
-pub struct SuccessUrl<'a>(&'a str);
+pub struct SuccessUrl<'a>(Cow<'a, str>);
 
 #[derive(Debug, Serialize, Deserialize, Default)]
 #[serde(transparent)]
-pub struct ReturnUrl<'a>(&'a str);
+pub struct ReturnUrl<'a>(Cow<'a, str>);
 
 impl<'a> From<&'a str> for CancelUrl<'a> {
     fn from(value: &'a str) -> Self {
-        Self(value)
+        Self(Cow::Borrowed(value))
+    }
+}
+
+impl From<String> for CancelUrl<'_> {
+    fn from(value: String) -> Self {
+        Self(Cow::Owned(value))
     }
 }
 
 impl<'a> From<&'a str> for SuccessUrl<'a> {
     fn from(value: &'a str) -> Self {
-        Self(value)
+        Self(Cow::Borrowed(value))
+    }
+}
+
+impl From<String> for SuccessUrl<'_> {
+    fn from(value: String) -> Self {
+        Self(Cow::Owned(value))
     }
 }
 
 impl<'a> From<&'a str> for ReturnUrl<'a> {
     fn from(value: &'a str) -> Self {
-        Self(value)
+        Self(Cow::Borrowed(value))
+    }
+}
+
+impl From<String> for ReturnUrl<'_> {
+    fn from(value: String) -> Self {
+        Self(Cow::Owned(value))
     }
 }
